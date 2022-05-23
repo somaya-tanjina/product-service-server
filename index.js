78621a0747d227b6a3bc6c8611a3f08865e331cd
somaya.tanjina <you@example.com>
2022-05-24 00:13:49 +0600

    data is loaded from dtabase

--- index.js
+++ index.js
@@ -3,27 +3,34 @@ const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 //middleware
 app.use(cors());
 app.use(express.json());
 
-//pass  E5tcBWOSJGWyiVv6
-//user manufacturer
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-    "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rk7co.mongodb.net/?retryWrites=true&w=majority";
-//console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rk7co.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client
+            .db("manufacturer")
+            .collection("products");
+        app.get("/products", async (req, res) => {
+            const query = {};
+            const cursor = await productCollection.find(query).toArray();
+            res.send(cursor);
+        });
+    } finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
